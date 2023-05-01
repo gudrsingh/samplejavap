3b800ea081533f8fb2eaf903d43d3a75e86b1ee4
gudrsingh <57560472+gudrsingh@users.noreply.github.com>
2023-05-01 17:24:52 +0530

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,7 @@
 public class hello {
 	public static void main (String[] args){
 		for (int i=1;i<=10;i++){
-		System.out.println("Hello Javap test sample1" +i);
+		System.out.println("Hello Javap test sample webhook" +i);
 		}
 	}
 }
